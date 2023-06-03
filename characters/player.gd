eb1c492748ef05fba980b5c63314d70f0b60ef5c
swordcube <49110074+swordcube@users.noreply.github.com>
2023-06-03 06:27:45 -0500

    i wanted to add multiplayer so i did\nit somehow works

---
+++ characters/player.gd
@@ -0,0 +1,149 @@
+class_name PlayerCharacter extends CharacterBody2D
+
+@export var SPEED = 80.0
+@export var BOOST_SPEED = 165.0
+
+@export var JUMP_VELOCITY = -500.0
+
+@onready var sprite := $sprite
+@onready var hitbox := $hitbox
+@onready var displayed_name := $displayed_name
+@onready var type_icon := $displayed_name/type
+@onready var camera:Camera2D = $"../../Camera2D"
+@onready var color_picker := $ColorPicker
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var on_floor:bool = false
+var ducking:bool = false
+
+var jump_hold:float = 0
+var quick_falling:bool = false
+
+# this is a shit way of storing player info
+# since it can be exploited
+# but i'm making this for fun anyways
+# if this turns into a full blown game then i'll
+# worry about this later on
+@export var player_name:String = "???"
+@export var player_id:int = -1
+@export var is_host:bool = false
+
+func _enter_tree():
+	set_multiplayer_authority(int(str(name)))
+
+func _ready():
+	displayed_name.modulate.a = 0.0
+	position = $"../../Level/StartPos".global_position
+	player_name = Global.player_name
+	player_id = $"../../Players".get_child_count()
+	is_host = player_id < 2
+	
+	if not is_multiplayer_authority(): return
+	sprite.play("jump" if !is_on_floor() else "idle")
+	camera.enabled = true
+	camera.make_current()
+	
+	color_picker.color = sprite.modulate
+	
+func _unhandled_key_input(event:InputEvent):
+	if not is_multiplayer_authority(): return
+	
+	if Input.is_action_just_pressed("change_color"):
+		color_picker.visible = not color_picker.visible
+
+func _process(delta):
+	displayed_name.text = "%s • #%s" % [player_name, str(player_id)]
+	displayed_name.size.x = 0
+	type_icon.play("host" if is_host else "player")
+	displayed_name.position.x = lerpf(displayed_name.position.x, displayed_name.size.x * -0.5, delta * 5.0)
+	displayed_name.modulate.a = lerpf(displayed_name.modulate.a, 1.0, delta * 5.0)
+	
+	color_picker.position.x = displayed_name.position.x + 15
+	color_picker.position.y = displayed_name.position.y - 95
+
+func _physics_process(delta):
+	if not is_multiplayer_authority(): return
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		if on_floor != is_on_floor():
+			sprite.play("jump")
+			on_floor = is_on_floor()
+			
+	elif on_floor != is_on_floor():
+		if quick_falling:
+			sprite.scale = Vector2(3, 1)
+			var tween = get_tree().create_tween()
+			tween.set_ease(Tween.EASE_OUT)
+			tween.set_trans(Tween.TRANS_BOUNCE)
+			tween.tween_property(sprite, "scale", Vector2(2, 2), 0.5)
+			
+		quick_falling = false
+		sprite.play("idle")
+		on_floor = is_on_floor()
+		
+	# Handle Duck (quack).
+	if !ducking and Input.is_action_just_pressed("duck") and is_on_floor():
+		ducking = true
+		sprite.play("duck")
+		hitbox.shape.size.y = 39
+		hitbox.position.y = 15 + (hitbox.shape.size.y * 0.5)
+		
+	if ducking and Input.is_action_just_released("duck") and is_on_floor():
+		ducking = false
+		sprite.play("idle")
+		hitbox.shape.size.y = 78
+		hitbox.position.y = 15
+		
+	# Quick Falling
+	if Input.is_action_just_pressed("duck") and not is_on_floor():
+		quick_falling = true
+		velocity.y = 850
+		sprite.play("quick_fall")
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jump_hold = 2.5
+		sprite.play("jump")
+		on_floor = false
+		
+	if Input.is_action_pressed("jump") and not on_floor:
+		velocity.y += JUMP_VELOCITY * (clampf(jump_hold, 0, 1) * 0.275)
+		jump_hold = lerpf(jump_hold, 0, 0.33)
+		
+	var play_walk_shit:bool = absf(velocity.x) > 50
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction:int = Input.get_axis("move_left", "move_right")
+	var boosting:bool = Input.is_action_pressed("boost")
+	velocity.x += direction * (BOOST_SPEED if boosting else SPEED)
+	sprite.speed_scale = (2.0 if boosting and on_floor and play_walk_shit else 1.0)
+	
+	if direction != 0:
+		sprite.flip_h = true if direction < 0 else false
+	
+	if on_floor:
+		if play_walk_shit:
+			ducking = false
+			sprite.play("walk")
+		elif not ducking:
+			sprite.play("idle")
+			
+	velocity.x *= 0.85
+	move_and_slide()
+	
+	camera.position = position
+	
+	var limit_x:float = 640
+	if position.x < limit_x:
+		camera.position.x = limit_x
+		
+	var limit_y:float = 310
+	if position.y > limit_y:
+		camera.position.y = limit_y
+
+func _on_color_picker_color_changed(color:Color):
+	sprite.modulate = color
--- scenes/Gameplay.gd
+++ scenes/Gameplay.gd
@@ -1,23 +1,87 @@
 extends Node2D
 
-@onready var level_container:Node = $LevelContainer
-@onready var camera:Camera2D = $Camera2D
-@onready var player:PlayerCharacter = $Player
+# multiplayer handling logic shits
+@onready var main_menu := $MultiplayerGUI/MainMenu
+@onready var username_entry := $MultiplayerGUI/MainMenu/MarginContainer/VBoxContainer/UsernameEntry
+@onready var address_entry := $MultiplayerGUI/MainMenu/MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/AddressEntry
+@onready var port_entry := $MultiplayerGUI/MainMenu/MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/PortEntry
+@onready var players := $Players
 
-var level:Node2D
+const PLAYER = preload("res://characters/player.tscn")
+
+var MULTIPLAYER_USERNAME:String = "player_invalid"
+var MULTIPLAYER_ADDRESS:String = "localhost"
+var MULTIPLAYER_PORT:int = 9999
+
+var enet_peer := ENetMultiplayerPeer.new()
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.WHITE)
-	level = load("res://scenes/levels/Level1.tscn").instantiate()
-	level_container.add_child(level)
-
-func _process(delta):
-	camera.position = player.position
 	
-	var limit_x:float = 580
-	if player.position.x < limit_x:
-		camera.position.x = limit_x
+func start_checking_invalid_state():
+	var timer = Timer.new()
+	timer.wait_time = 1.0
+	timer.one_shot = false
+	timer.timeout.connect(check_invalid_state)
+	add_child(timer)
+	timer.start()
+	
+func check_invalid_state():
+	if players.get_child_count() < 1:
+		Global.last_error = "The server host has disconnected!"
+		get_tree().change_scene_to_file("res://scenes/menus/ErrorScreen.tscn")
+	
+func _on_host_button_pressed():
+	main_menu.hide()
+	
+	Global.player_name = username_entry.text if len(username_entry.text) > 0 else "player_%s" % str(randi_range(0, 99999))
+	MULTIPLAYER_ADDRESS = address_entry.text if len(address_entry.text) > 0 else "localhost"
+	MULTIPLAYER_PORT = int(port_entry.text) if len(port_entry.text) > 0 else 9999
+	
+	enet_peer.create_server(MULTIPLAYER_PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+	add_player(multiplayer.get_unique_id())
+	
+	upnp_setup()
+	start_checking_invalid_state()
+
+func _on_join_button_pressed():
+	Global.player_name = username_entry.text if len(username_entry.text) > 0 else "player_%s" % str(randi_range(0, 99999))
+	main_menu.hide()
+	enet_peer.create_client(MULTIPLAYER_ADDRESS, MULTIPLAYER_PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	start_checking_invalid_state()
+
+func add_player(peer_id:int):
+	print("Player of ID %s (Peer ID: %s) joined!" % [players.get_child_count() + 1, str(peer_id)])
+	var player = PLAYER.instantiate()
+	player.name = str(peer_id)
+	players.add_child(player)
+	
+func remove_player(peer_id:int):
+	var player:PlayerCharacter = players.get_node_or_null(str(peer_id))
+	if player != null:
+		print("Player of ID %s (Peer ID: %s) joined!" % [players.get_child_count(), str(peer_id)])
+		player.queue_free()
 		
-	var limit_y:float = 350
-	if player.position.y > limit_y:
-		camera.position.y = limit_y
+func upnp_setup():
+	var upnp := UPNP.new()
+	var discover_result:int = upnp.discover()
+	
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Discover Failed! Error %s" % discover_result)
+	
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
+		"UPNP Invalid Gateway!")
+	
+	var map_result:int = upnp.add_port_mapping(MULTIPLAYER_PORT)
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Port Mapping Failed! Error %s" % discover_result)
+		
+	print("UPNP Success! Join address: %s" % upnp.query_external_address())
+	
+# local shits
+@onready var level:Node2D = $Level
+@onready var camera:Camera2D = $Camera2D
