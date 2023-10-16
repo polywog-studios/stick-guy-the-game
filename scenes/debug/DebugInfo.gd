a4f16c9dc7ede94f6cf068e2fb095aabd9c3982b
Raf <85767304+RafGamign@users.noreply.github.com>
2023-10-15 22:06:37 -0300

    limit fps + show ram

--- scenes/debug/DebugInfo.gd
+++ scenes/debug/DebugInfo.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	if not OS.is_debug_build():
 		queue_free()
 		return
+	_update()
 
 func _update() -> void:
-	balls.text = '%s fps' % [Engine.get_frames_per_second()]
+	balls.text = '%s fps - %smb' % [Engine.get_frames_per_second(), "%.2f" % (OS.get_static_memory_usage()/1000000.0)]
