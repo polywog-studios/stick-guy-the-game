d28bc8fec3ac47c081ba44cf5599dbd3c436aef5
swordcube <swordsoftarchive@gmail.com>
2024-02-11 22:53:54 -0600

    if you hold duck while deaccelerating, you will duck as soon as you idle

--- components/game/player_states/PlayerIdleState.gd
+++ components/game/player_states/PlayerIdleState.gd
@@ -1,11 +1,16 @@
 class_name PlayerIdleState extends PlayerState
 
+var ducking:bool = false
+
 func on_enter():
+	ducking = false
 	sprite.speed_scale = 1.0
 	sprite.play(&"Idle")
 
 func on_physics_process(_delta:float):
-	if Input.is_action_just_pressed(&"duck"):
+	if Input.is_action_pressed(&"duck") and not ducking:
+		ducking = true
 		sprite.play(&"Duck")
 	elif Input.is_action_just_released(&"duck"):
+		ducking = false
 		sprite.play(&"Idle")
